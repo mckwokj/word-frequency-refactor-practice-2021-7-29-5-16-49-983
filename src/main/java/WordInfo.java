711ee4bd4db591e67a1253b6f120595b30f92503
Johnson Kwok <kwokjo2@oocl.com>
2021-12-06 16:41:11 +0800

    [WordFrequencyGame] [Johnson Kwok] refactor: rename w to word

--- src/main/java/WordInfo.java
+++ src/main/java/WordInfo.java
@@ -2,8 +2,8 @@ public class WordInfo {
     private String value;
     private int count;
 
-    public WordInfo(String w, int i){
-        this.value =w;
+    public WordInfo(String word, int i){
+        this.value =word;
         this.count =i;
     }
 
