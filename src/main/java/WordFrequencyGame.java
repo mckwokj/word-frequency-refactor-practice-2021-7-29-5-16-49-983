0c19c14ae0febd8c27fbd138191c1feb1e356e43
Johnson Kwok <kwokjo2@oocl.com>
2021-12-07 10:02:44 +0800

    [WordFrequencyGame] [Johnson Kwok] refactor: put new line character to Collectors.joining

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,21 +6,20 @@ public class WordFrequencyGame {
     private static final String SPACE_PATTERN = "\\s+";
     public static final String CALCULATE_ERROR_EXCEPTION_MSG = "Calculate Error";
 
-
     public String getWordFrequencyResult(String sentence) {
         try {
             List<WordInfo> wordInfos = calculateWordFrequency(sentence);
             return wordInfos.stream()
                     .sorted(Comparator.comparingInt(WordInfo::getWordCount).reversed())
                     .map(wordInfo -> getWordInfoLine(wordInfo))
-                    .collect(Collectors.joining()).trim();
+                    .collect(Collectors.joining("\n"));
         } catch (Exception e) {
             return CALCULATE_ERROR_EXCEPTION_MSG;
         }
     }
 
     private String getWordInfoLine(WordInfo wordInfo) {
-        return wordInfo.getWord() + " " + wordInfo.getWordCount() + "\n";
+        return wordInfo.getWord() + " " + wordInfo.getWordCount();
     }
 
     private List<WordInfo> calculateWordFrequency(String sentence) {
